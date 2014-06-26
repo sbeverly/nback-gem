cfec30fa711bf099c68096a2594dc737a57c7250
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-25 20:40:28 -0400

    add NbackGame#generate_rounds and add Round class with initialize method

--- lib/nback_game.rb
+++ lib/nback_game.rb
@@ -5,4 +5,20 @@ class NbackGame
 		@round_attributes = round_attributes
 		@rounds = []
 	end
+
+	def generate_rounds
+		@n + 20.times do
+			@rounds << Round.new(round_number, @round_attributes)
+		end
+	end
+end
+
+
+class Round
+	def initialize(round_number, round_attributes)
+		@round_number = round_number
+		@color = select_color(round_attributes[:color])
+		@sound = select_sound(round_attributes[:sound])
+		@position = select_position(round_attributes[:position])
+	end
 end
