b8d764f2c665ab1fbdcd0579e4e9cffeb6fb547b
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-28 00:30:38 -0400

    add logic to evaluate non responses in rounds less than n accurately - remove debuggin statements - fix round numbering in NbackGame#generate_rounds

--- lib/nback_game.rb
+++ lib/nback_game.rb
@@ -7,8 +7,8 @@ class NbackGame
 	end
 
 	def generate_rounds
+		@round_number = 1
 		@n + 20.times do
-			@round_number = 0
 			@rounds << Round.new(@round_number, @round_attributes)
 			@round_number += 1
 		end
@@ -17,9 +17,6 @@ class NbackGame
 	def evaluate_users_guess(current_round_number, attribute)
 		@current_round = @rounds[current_round_number - 1]
 		@nback_round = @rounds[current_round_number - 1 - @n]
-
-		puts @current_round.round_attributes[:color]
-		puts @nback_round.round_attributes[:color]
 
 		if current_round_number > @n
 			if @current_round.round_attributes[attribute.to_sym] == @nback_round.round_attributes[attribute.to_sym] 
@@ -32,24 +29,32 @@ class NbackGame
 		end
 	end
 
-	def evaluate_non_response(current_round)
-		@current_round = @rounds[current_round - 1]
-		@nback_round = @rounds[current_round - 1 - @n]
+	def evaluate_non_response(current_round_number)
+		@current_round = @rounds[current_round_number - 1]
+		@nback_round = @rounds[current_round_number - 1 - @n]
 
-		@round_attributes.each_key do |attribute|
-			if @current_round.round_attributes["#{attribute}_correct".to_sym] == nil
-				if @current_round.round_attributes[attribute.to_sym] != @nback_round.round_attributes[attribute.to_sym]
-					@current_round.round_attributes["#{attribute}_correct".to_sym] = true
-				else
-					@current_round.round_attributes["#{attribute}_correct".to_sym] = false
+		if current_round_number < @n
+			@round_attributes.each_key do |attribute|
+				if @current_round.round_attributes["#{attribute}_correct".to_sym] == nil
+					if @current_round.round_attributes[attribute.to_sym] != @nback_round.round_attributes[attribute.to_sym]
+						@current_round.round_attributes["#{attribute}_correct".to_sym] = true
+					end
+				end
+			end
+		else
+			@round_attributes.each_key do |attribute|
+				if @current_round.round_attributes["#{attribute}_correct".to_sym] == nil
+					if @current_round.round_attributes[attribute.to_sym] != @nback_round.round_attributes[attribute.to_sym]
+						@current_round.round_attributes["#{attribute}_correct".to_sym] = true
+					else
+						@current_round.round_attributes["#{attribute}_correct".to_sym] = false
+					end
 				end
 			end
 		end
-		return @current_round
 	end
 
 	def show_round_attributes(round_number)
-		puts @rounds[round_number - 1].round_attributes
 		@rounds[round_number - 1].round_attributes
 	end 
 end
@@ -59,10 +64,9 @@ class Round
 
 	def initialize(round_number, round_attributes)
 		@round_number = round_number
-		@round_attributes = { 	color: round_attributes[:color].sample, color_correct: nil, 
-								sound: round_attributes[:sound].sample, sound_correct: nil,
-								position: round_attributes[:position].sample, position_correct: nil
-							}
-		p @round_attributes[:color]
+		@round_attributes = { color: round_attributes[:color].sample, color_correct: nil, 
+													sound: round_attributes[:sound].sample, sound_correct: nil,
+													position: round_attributes[:position].sample, position_correct: nil
+												}
 	end
 end
