96300d2a8674b4f0bb0a916f6a9e86ffe29b14a1
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-27 22:57:14 -0400

    add logic to automatically record an incorrect guess if user indicates a match before n rounds have passed

--- lib/nback_game.rb
+++ lib/nback_game.rb
@@ -14,15 +14,19 @@ class NbackGame
 		end
 	end
 
-	def evaluate_users_guess(current_round, attribute)
-		@current_round = @rounds[current_round - 1]
-		@nback_round = @rounds[current_round - 1 - @n]
+	def evaluate_users_guess(current_round_number, attribute)
+		@current_round = @rounds[current_round_number - 1]
+		@nback_round = @rounds[current_round_number - 1 - @n]
 
 		puts @current_round.round_attributes[:color]
 		puts @nback_round.round_attributes[:color]
 
-		if @current_round.round_attributes[attribute.to_sym] == @nback_round.round_attributes[attribute.to_sym] 
-			@current_round.round_attributes["#{attribute}_correct".to_sym] = true
+		if current_round_number > @n
+			if @current_round.round_attributes[attribute.to_sym] == @nback_round.round_attributes[attribute.to_sym] 
+				@current_round.round_attributes["#{attribute}_correct".to_sym] = true
+			else
+				@current_round.round_attributes["#{attribute}_correct".to_sym] = false
+			end
 		else
 			@current_round.round_attributes["#{attribute}_correct".to_sym] = false
 		end
