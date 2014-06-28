74a28b5d710b324d7c50147d6bee53ed56c496ef
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-27 22:49:29 -0400

    refactor NbackGame#evaluate_users_guess

--- lib/nback_game.rb
+++ lib/nback_game.rb
@@ -22,23 +22,9 @@ class NbackGame
 		puts @nback_round.round_attributes[:color]
 
 		if @current_round.round_attributes[attribute.to_sym] == @nback_round.round_attributes[attribute.to_sym] 
-			case attribute
-			when 'color'
-				@current_round.round_attributes[:color_correct] = true
-			when 'sound'
-				@current_round.round_attributes[:sound_correct] = true
-			when 'position'
-				@current_round.round_attributes[:position_correct] = true
-			end
+			@current_round.round_attributes["#{attribute}_correct".to_sym] = true
 		else
-			case attribute
-			when 'color'
-				@current_round.round_attributes[:color_correct] = false
-			when 'sound'
-				@current_round.round_attributes[:sound_correct] = false
-			when 'position'
-				@current_round.round_attributes[:position_correct] = false
-			end
+			@current_round.round_attributes["#{attribute}_correct".to_sym] = false
 		end
 	end
 
@@ -51,7 +37,7 @@ class NbackGame
 				if @current_round.round_attributes[attribute.to_sym] != @nback_round.round_attributes[attribute.to_sym]
 					@current_round.round_attributes["#{attribute}_correct".to_sym] = true
 				else
-				@current_round.round_attributes["#{attribute}_correct".to_sym] = false
+					@current_round.round_attributes["#{attribute}_correct".to_sym] = false
 				end
 			end
 		end
@@ -69,10 +55,10 @@ class Round
 
 	def initialize(round_number, round_attributes)
 		@round_number = round_number
-		@round_attributes = { color: round_attributes[:color].sample, color_correct: nil, 
-													sound: round_attributes[:sound].sample, sound_correct: nil,
-													position: round_attributes[:position].sample, position_correct: nil
-												}
+		@round_attributes = { 	color: round_attributes[:color].sample, color_correct: nil, 
+								sound: round_attributes[:sound].sample, sound_correct: nil,
+								position: round_attributes[:position].sample, position_correct: nil
+							}
 		p @round_attributes[:color]
 	end
 end
