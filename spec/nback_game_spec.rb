4e93bd66f641c1df40ab1fc565115c703775f911
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-27 22:33:38 -0400

    use to eq syntax in rspec

--- spec/nback_game_spec.rb
+++ spec/nback_game_spec.rb
@@ -5,18 +5,16 @@ describe "NBackGame" do
 		@new_game = NbackGame.new(2, 'single', { color: ["blue", "red", "green", "orange"], sound: ["/sounds/cat.mp3", "/sounds/dog.mp3", "/sounds/cat.mp3"], position: [1, 2, 1, 4]})
 	end
 
-	context 'when a user is presented with an nback match' do
-		it "records a correct guess if the user responds" do
-			@new_game.generate_rounds
-			@round = 2
+	it "records a correct guess if the user responds" do
+		@new_game.generate_rounds
+		@round = 2
 
-			until @response == true
-				@round += 1
-				@response = @new_game.evaluate_users_guess(@round, 'color')
-			end
-
-			expect(@new_game.show_round_attributes(@round)[:color]) ===  @new_game.show_round_attributes(@round - 2)[:color]
-			expect(@new_game.show_round_attributes(@round)[:color_correct]).to be(true)
+		until @response == true
+			@round += 1
+			@response = @new_game.evaluate_users_guess(@round, 'color')
 		end
+
+		expect(@new_game.show_round_attributes(@round)[:color]).to eq(@new_game.show_round_attributes(@round - 2)[:color])
+		expect(@new_game.show_round_attributes(@round)[:color_correct]).to eq(true)
 	end
 end
